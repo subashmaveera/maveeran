952978b053998eeff50f60677552772aded3e21e
subashmaveera <subashm11660@gmail.com>
2019-05-08 10:35:19 +0530

    name added

---
+++ src/sample/String.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("my name is subash");
+	}
+
+}
